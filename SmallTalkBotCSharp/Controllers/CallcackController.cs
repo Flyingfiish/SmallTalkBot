4658844246452b8619e1e616f470e3fbd7d19c87
Eldar <eldarg76@gmail.com>
2019-08-13 20:26:44 +0500

    dialogflow

--- SmallTalkBotCSharp/Controllers/CallcackController.cs
+++ SmallTalkBotCSharp/Controllers/CallcackController.cs
@@ -5,6 +5,8 @@ using VkNet.Abstractions;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
 using VkNet.Utils;
+using Google.Cloud.Dialogflow.V2;
+using System.Text;
 
 namespace SmallTalkBotCSharp.Controllers
 {
@@ -38,19 +40,59 @@ namespace SmallTalkBotCSharp.Controllers
                     {
                         // Десериализация
                         var msg = Message.FromJson(new VkResponse(updates.Object));
-
+                        string answer = DetectIntentFromTexts("smalltalkbot - atvxnh", Guid.NewGuid().ToString(), msg.Text.Split(' '));
                         // Отправим в ответ полученный от пользователя текст
                         _vkApi.Messages.Send(new MessagesSendParams
                         {
                             RandomId = new DateTime().Millisecond,
                             PeerId = msg.PeerId.Value,
-                            Message = msg.Text
+                            Message = answer
+                            
                         });
                         break;
                     }
             }
             // Возвращаем "ok" серверу Callback API
             return Ok("ok");
+        }
+
+        public static string DetectIntentFromTexts(string projectId,
+                                                string sessionId,
+                                                string[] texts,
+                                                string languageCode = "ru")
+        {
+            var client = SessionsClient.Create();
+
+            StringBuilder answer = new StringBuilder();
+
+            foreach (var text in texts)
+            {
+                var response = client.DetectIntent(
+                    session: new SessionName(projectId, sessionId),
+                    queryInput: new QueryInput()
+                    {
+                        Text = new TextInput()
+                        {
+                            Text = text,
+                            LanguageCode = languageCode
+                        }
+                    }
+                );
+
+                var queryResult = response.QueryResult;
+
+                answer.Append(" " + queryResult.FulfillmentText);
+
+                //Console.WriteLine($"Query text: {queryResult.QueryText}");
+                //if (queryResult.Intent != null)
+                //{
+                //    Console.WriteLine($"Intent detected: {queryResult.Intent.DisplayName}");
+                //}
+                //Console.WriteLine($"Intent confidence: {queryResult.IntentDetectionConfidence}");
+                //Console.WriteLine($"Fulfillment text: {queryResult.FulfillmentText}");
+            }
+
+            return answer.ToString();
         }
     }
 }
